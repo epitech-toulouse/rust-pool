4cdf58c61b7530814151251284969007bece2d37
loic-rouzaud <loic.rouzaud@epitech.eu>
2025-05-20 13:50:32 +0200

    fix : errors in solutions

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,7 @@ mod exercices;
 mod solutions;
 
 fn main() {
-    println!("Hello");
+    println!("Bienvenue !\nexercices made by Loïc Rouzaud Epitech TOULOUSE\n");
+    println!("Usage :\n\tCompiler chaque fichier avec la commande rustc file_name && ./filename");
+    println!("\tPour les fonctions qui ont des tests : rustc filename --test && ./filename");
 }
