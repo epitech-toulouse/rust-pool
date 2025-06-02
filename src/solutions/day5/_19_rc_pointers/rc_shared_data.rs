a6a9913ed4e28a7ff58fca8bcbeeea148b5eb720
loic-rouzaud <loic.rouzaud@epitech.eu>
2025-06-02 14:14:39 +0200

    fix : rc_shared_data bc tests were wrong

--- src/solutions/day5/_19_rc_pointers/rc_shared_data.rs
+++ src/solutions/day5/_19_rc_pointers/rc_shared_data.rs
@@ -2,13 +2,7 @@ use std::rc::Rc;
 
 fn create_shared_resource() -> Rc<Vec<i32>> {
     let my_vec = vec![1, 2, 3, 4, 5];
-    let shared_data = Rc::new(my_vec);
-    shared_data
-}
-
-fn add_consumer(resource: &Rc<Vec<i32>>) -> usize {
-    let _consumer = Rc::clone(resource);
-    Rc::strong_count(resource)
+    Rc::new(my_vec)
 }
 
 fn main() {
@@ -18,17 +12,22 @@ fn main() {
         Rc::strong_count(&original_vec)
     );
 
+    let consumer1 = Rc::clone(&original_vec);
     println!(
         "Après 1er consommateur: {}",
-        add_consumer(&Rc::clone(&original_vec))
+        Rc::strong_count(&original_vec)
     );
+
+    let consumer2 = Rc::clone(&original_vec);
     println!(
         "Après 2ème consommateur: {}",
-        add_consumer(&Rc::clone(&original_vec))
+        Rc::strong_count(&original_vec)
     );
+
+    let consumer3 = Rc::clone(&original_vec);
     println!(
         "Après 3ème consommateur: {}",
-        add_consumer(&Rc::clone(&original_vec))
+        Rc::strong_count(&original_vec)
     );
 
     {
@@ -47,8 +46,11 @@ fn main() {
 fn test_shared_resource() {
     let original_vec = create_shared_resource();
     assert_eq!(Rc::strong_count(&original_vec), 1);
-    let count1 = add_consumer(&original_vec);
-    assert_eq!(count1, 2); // ⚠️ ATTENTION: Le test actuel a une erreur, il devrait être 2 et non 3
-    let count2 = add_consumer(&original_vec);
-    assert_eq!(count2, 3);
+
+    let consumer1 = Rc::clone(&original_vec);
+    assert_eq!(Rc::strong_count(&original_vec), 2);
+
+    let consumer2 = Rc::clone(&original_vec);
+    assert_eq!(Rc::strong_count(&original_vec), 3);
+    assert_eq!(*original_vec, vec![1, 2, 3, 4, 5]);
 }
